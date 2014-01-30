2265bbd0d3ebe2871855350b7e63add623e17e4e
Сергей Волков <sv.volkoff@gmail.com>
2014-01-31 00:39:17 +0400

    Handle And Event

---
+++ HandleAndEvent/HandleAndEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.HandleAndEvent
+{
+    class HandleAndEvent
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+
+            Window win = new Window();
+            win.Title = "Handle And Event";
+            win.MouseDown += WindowOnMouseDown;
+
+            app.Run(win);
+        }
+
+        static void WindowOnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            Window win = sender as Window;
+            string strMessage = string.Format("Window clicked with {0} button at point {1}", args.ChangedButton,
+                                              args.GetPosition(win));
+            MessageBox.Show(strMessage, win.Title);
+        }
+    }
+}
