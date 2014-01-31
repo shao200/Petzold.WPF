cb721f0c334ad6a1572a06204cdac373d948d1a4
Сергей Волков <sv.volkoff@gmail.com>
2014-01-31 20:53:01 +0400

    Page 22.

---
+++ InheriTheApp/InheritTheApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.InheritTheApp
+{
+    class InheritTheApp : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            InheritTheApp app = new InheritTheApp();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window win = new Window();
+            win.Title = "Inherit the App";
+            win.Show();
+        }
+
+        protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+        {
+            base.OnSessionEnding(e);
+            MessageBoxResult result = MessageBox.Show("Do you want to save your data?", MainWindow.Title,
+                                                      MessageBoxButton.YesNoCancel, MessageBoxImage.Question,
+                                                      MessageBoxResult.Yes);
+        }
+    }
+}
---
+++ ThrowWindowParty/ThrowWindowParty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Petzold.ThrowWindowParty
+{
+    class ThrowWindowParty : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            ThrowWindowParty app = new ThrowWindowParty();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window winMain = new Window();
+            winMain.Title = "Main Window";
+            winMain.MouseDown += WindowOnMouseDown;
+            winMain.Show();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Window win = new Window();
+                win.Title = "Extra window No. " + (i + 1);
+                win.Show();
+            }
+        }
+
+        void WindowOnMouseDown(object sender, MouseButtonEventArgs args)
+        {
+            Window win = new Window();
+            win.Title = "Modal Dialog Box";
+            win.ShowDialog();
+        }
+    }
+
+}
