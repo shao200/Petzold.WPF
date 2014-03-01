1e4154d4eabb0d71f9282c0e1fd2d022f21894be
Сергей Волков <sv.volkoff@gmail.com>
2014-03-01 23:07:15 +0400

    DisplaySomeText()

---
+++ RotateTheGradientOrigin/RotateTheGradientOrigin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Petzold.RotateTheGradientOrigin
+{
+    class RotateTheGradientOrigin : Window
+    {
+        private RadialGradientBrush brush;
+        private double angle;
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new RotateTheGradientOrigin());
+        }
+
+        public RotateTheGradientOrigin()
+        {
+            Title = "Rotate The Gradient Origin";
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            Width = 384;
+            Height = 384;
+
+            brush = new RadialGradientBrush(Colors.White, Colors.Blue);
+            brush.Center = brush.GradientOrigin = new Point(0.5, 0.5);
+            brush.RadiusX = brush.RadiusY = 0.1;
+            brush.SpreadMethod = GradientSpreadMethod.Repeat;
+            Background = brush;
+
+            BorderBrush = Brushes.SaddleBrown;
+            BorderThickness = new Thickness(25, 50, 75, 100);
+
+            DispatcherTimer tmr = new DispatcherTimer();
+            tmr.Interval = TimeSpan.FromMilliseconds(100);
+            tmr.Tick += TimerOnTick;
+            tmr.Start();
+        }
+
+        void TimerOnTick(object sender, EventArgs e)
+        {
+            Point pt = new Point(0.5 + 0.05 * Math.Cos(angle), 0.5 + 0.05 * Math.Sin(angle));
+            brush.GradientOrigin = pt;
+            angle += Math.PI / 6;
+        }
+    }
+}
