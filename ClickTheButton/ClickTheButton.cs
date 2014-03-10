df1b99e7ae616ee33e539745262de71cf0280b3d
Сергей Волков <sv.volkoff@gmail.com>
2014-03-10 22:34:40 +0400

    FormatTheButton. Page 77.

---
+++ ClickTheButton/ClickTheButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ClickTheButton
+{
+    public class ClickTheButton : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ClickTheButton());
+        }
+
+        public ClickTheButton()
+        {
+            Title = "Click The Button";
+
+            Button btn = new Button();
+            btn.Content = "_Click me, please!";
+            btn.Click += ButtonOnClick;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Width = 96;
+            btn.Height = 96;
+
+            btn.FontSize = 48;
+            btn.FontFamily = new FontFamily("Times New Roman");
+
+            btn.Background = Brushes.AliceBlue;
+            btn.Foreground = Brushes.DarkSalmon;
+            btn.BorderBrush = Brushes.Magenta;
+
+            Content = btn;
+        }
+
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("The button has been clicked and all is well.", Title);
+        }
+    }
+}
